97bfb78ceecc32215a3c64e2ce86f5c38639cf98
Maxim Lopatnikov <maxim.alp@gmail.com>
2018-01-04 00:06:01 -0500

    heroku mongo fix

--- server.js
+++ server.js
@@ -42,10 +42,13 @@ app.set("view engine", "handlebars");
 //app.use("/", routes);
 
 
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/MongoScraperDB", {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
